8810ea18bdf94e55c3e14755b747ebeb15056130
Fernando Garcia Alvarez <fercarcedo@gmail.com>
2017-11-04 12:55:47 +0100

    Fixed some lint warnings

--- app/src/main/java/fergaral/datetophoto/receivers/BootReceiver.kt
+++ app/src/main/java/fergaral/datetophoto/receivers/BootReceiver.kt
@@ -10,8 +10,10 @@ import fergaral.datetophoto.jobs.ProcessPhotosJob
  * Created by Parejúa on 24/07/2014.
  */
 class BootReceiver : BroadcastReceiver() {
-
     override fun onReceive(context: Context, intent: Intent) {
-        ProcessPhotosJob.scheduleNow()
+        if (intent.action in arrayOf("android.intent.action.BOOT_COMPLETED",
+                "android.intent.action.QUICKBOOT_POWERON")) {
+            ProcessPhotosJob.scheduleNow()
+        }
     }
 }
