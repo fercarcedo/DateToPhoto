fe522be9e60a55e80c041e10798bffa4d83c040d
Fernando Garcia Alvarez <fercarcedo@gmail.com>
2019-01-28 00:55:30 +0100

    Migrate to MLKit

--- app/src/main/java/fergaral/datetophoto/algorithms/VisionAPIPhotoProcessedAlgorithm.kt
+++ app/src/main/java/fergaral/datetophoto/algorithms/VisionAPIPhotoProcessedAlgorithm.kt
@@ -1,48 +1,44 @@
 package fergaral.datetophoto.algorithms
 
-import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Rect
-import android.util.SparseArray
-
-import com.google.android.gms.vision.Frame
-import com.google.android.gms.vision.text.TextBlock
-import com.google.android.gms.vision.text.TextRecognizer
-
-import java.io.File
-
-import fergaral.datetophoto.DateToPhoto
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import fergaral.datetophoto.utils.Point
+import java.io.File
+import java.util.concurrent.ExecutionException
 
 /**
  * Created by Fer on 18/10/2017.
  */
 
 class VisionAPIPhotoProcessedAlgorithm : PhotoProcessedAlgorithm {
+    private val detector by lazy {
+        FirebaseVision.getInstance()
+            .onDeviceTextRecognizer
+    }
+
     override fun isProcessed(photoPath: String): PhotoProcessedAlgorithm.Result {
-        val context = DateToPhoto.instance
         val options = BitmapFactory.Options()
         options.inSampleSize = 2
         val imageBitmap = BitmapFactory.decodeFile(File(photoPath).absolutePath, options) ?: return PhotoProcessedAlgorithm.Result.ERROR
-        val textRecognizer = TextRecognizer.Builder(context).build()
+        val firebaseImage = FirebaseVisionImage.fromBitmap(imageBitmap)
         var datestamped = false
 
-        if (textRecognizer.isOperational) {
-            val frame = Frame.Builder()
-                    .setBitmap(imageBitmap)
-                    .build()
-
-            val textBlocks = textRecognizer.detect(frame)
-
-            for (i in 0 until textBlocks.size()) {
-                val boundingBox = textBlocks.valueAt(i).boundingBox
-                val bottomRightPoint = Point(boundingBox.right, boundingBox.bottom)
-                if (bottomRightPoint.isInBottomRightCorner(imageBitmap.width, imageBitmap.height)) {
-                    datestamped = true
-                    break
+        try {
+            val recognizedText = Tasks.await(detector.processImage(firebaseImage))
+            for (i in 0 until recognizedText.textBlocks.size) {
+                val boundingBox = recognizedText.textBlocks[i].boundingBox
+                if (boundingBox != null) {
+                    val bottomRightPoint = Point(boundingBox.right, boundingBox.bottom)
+                    if (bottomRightPoint.isInBottomRightCorner(imageBitmap.width, imageBitmap.height)) {
+                        datestamped = true
+                        break
+                    }
                 }
             }
+        } catch (e: ExecutionException) {
+        } catch (e: InterruptedException) {
         }
 
         return if (datestamped) PhotoProcessedAlgorithm.Result.PROCESSED else PhotoProcessedAlgorithm.Result.NOT_PROCESSED
