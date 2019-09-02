f1bf68421678464f81798fd772c86ad83f81e511
Fernando Garcia Alvarez <fercarcedo@gmail.com>
2019-09-02 15:50:47 +0200

    Edge to edge storage permission activity

--- app/src/main/java/fergaral/datetophoto/activities/StoragePermissionDeniedActivity.kt
+++ app/src/main/java/fergaral/datetophoto/activities/StoragePermissionDeniedActivity.kt
@@ -1,19 +1,15 @@
 package fergaral.datetophoto.activities
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.provider.Settings
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.view.ViewCompat
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
 
 import fergaral.datetophoto.R
-import fergaral.datetophoto.utils.Utils
 
 class StoragePermissionDeniedActivity : StoragePermissionBaseActivity() {
 
@@ -21,10 +17,34 @@ class StoragePermissionDeniedActivity : StoragePermissionBaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_storage_permission_denied)
 
+        val container = findViewById<View>(R.id.container)
+        container.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        handleContainerInsets(container)
         val permissionBtn = findViewById<View>(R.id.permissionBtn) as Button
-        permissionBtn.setOnClickListener { _ -> requestPermissionClicked() }
+        handlePermissionButtonInsets(permissionBtn)
+        permissionBtn.setOnClickListener { requestPermissionClicked() }
     }
 
+    private fun handleContainerInsets(container: View) {
+        ViewCompat.setOnApplyWindowInsetsListener(container) { v, insets ->
+            v.updatePadding(
+                    top = insets.systemWindowInsetTop,
+                    left = insets.systemWindowInsetLeft,
+                    right = insets.systemWindowInsetRight
+            )
+            insets
+        }
+    }
+
+    private fun handlePermissionButtonInsets(permissionButton: Button) {
+        ViewCompat.setOnApplyWindowInsetsListener(permissionButton) { v, insets ->
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                bottomMargin = insets.systemWindowInsetBottom
+            }
+            insets
+        }
+    }
 
     override fun onBackPressed() {
         finish() //Since it's the first screen of the app, we simply leave it
